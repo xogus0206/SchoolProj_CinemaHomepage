75685440997cd4044bb0e79e01ab6edca43a98d6
YTH <92296499+xogus0206@users.noreply.github.com>
2022-05-20 16:54:14 +0900

    Delete Cinema directory

--- Cinema/reserve2.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-	var temp = document.getElementById('keytitle').innerText;
-	var occupied = new Array(); 
-	occupied = window.localStorage[temp].split(',');
-	for (var i = 0; i < occupied.length; i++) {
-		document.getElementById(occupied[i]).className = 'seat not_able';
-	}
-}
